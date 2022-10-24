d76c4f6c9d105c1038c13920518f5b88b999f67d
Angelo31294 <luongoangelo1@gmail.com>
2022-10-24 16:56:09 +0200

    Agg. multipli di 3 & 5 + stampa + stampa FizzBuzz

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 // 1. Programma con una lista di numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    if(i % 3 == 0)                                      // 1a. per i multipli di "3" stampa Fizz
+    if(i % 3 == 0)                                     // 1a. per i multipli di "3" stampa Fizz
     console.log("Fizz");
-    else if(i % 5 == 0)                                 // 1b. per i multipli di "5" stampa Buzz
-    console.log("Buzz");                                
-
+    else if(i % 5 == 0)                                // 1b. per i multipli di "5" stampa Buzz
+    console.log("Buzz");
+    else(1 % 3 == 0 && 1 % 5 == 0 )                    // 1b. per i multipli di "3" e "5" stampa FizzBuzz
+    console.log("FizzBuzz");
+    console.log(i);
 }
