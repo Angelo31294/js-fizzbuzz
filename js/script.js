76cdeeedbe096f2d89d5506f82aecb95565de9b1
Angelo31294 <luongoangelo1@gmail.com>
2022-10-24 16:38:11 +0200

    Agg. programma con lista da 1 a 100

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// 1. Programma con una lista di numeri da 1 a 100
+for (let i = 1; i <= 100; i++)
+console.log(i)
+// 1a. per i multipli di "3" stampa Frizz
+// 1b. per i multipli di "5" stampa Buzz
