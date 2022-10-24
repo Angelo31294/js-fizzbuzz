aa1a9d0e544738b2fbd1024746cc0a5e86d2051d
Angelo31294 <luongoangelo1@gmail.com>
2022-10-24 16:52:50 +0200

    Agg. i multipli di 5 e la stampa Buzz

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 // 1. Programma con una lista di numeri da 1 a 100
-for (let i = 1; i <= 100; i++)
-console.log(i)
-// 1a. per i multipli di "3" stampa Frizz
-// 1b. per i multipli di "5" stampa Buzz
+for (let i = 1; i <= 100; i++) {
+    if(i % 3 == 0)                                      // 1a. per i multipli di "3" stampa Fizz
+    console.log("Fizz");
+    else if(i % 5 == 0)                                 // 1b. per i multipli di "5" stampa Buzz
+    console.log("Buzz");                                
+
+}
