874830a909b2766bc737a0f976b52c7d20d95dcc
Angelo31294 <luongoangelo1@gmail.com>
2022-10-25 10:27:59 +0200

    Correzione

--- js/script.js
+++ js/script.js
@@ -1,27 +1,15 @@
-// Bonus 1
-const ul = document.querySelector("ul")
+"use strict"
 
 // 1. Programma con una lista di numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    if(i % 3 == 0){                                   // 1a. per i multipli di "3" stampa Fizz
-    console.log("Fizz");
-    let element = document.createElement(`li`);
-    element.classList.add(`box-${i}`);
-    element.innerHTML = console.log("i");
-    ul.append(element);
-    }
-    if(i % 5 == 0){                                // 1b. per i multipli di "5" stampa Buzz
-    console.log("Buzz");
-    let element = document.createElement(`li`);
-    element.classList.add(`box-${i}`);
-    element.innerHTML = i;
-    ul.append(element);
-    }
-    else(1 % 3 == 0 && 1 % 5 == 0 )                    // 1c. per i multipli di "3" e "5" stampa FizzBuzz
+    
+    if(i % 3 === 0 && i % 5 === 0){                       // 1a. per i multipli di "3" stampa Fizz
     console.log("FizzBuzz");
-    console.log(i);
-    let element = document.createElement(`li`);
-    element.classList.add(`box-${i}`);
-    element.innerHTML = i;
-    ul.append(element);                                // Bonus 1
+    }else if(i % 3 === 0){                                // 1b. per i multipli di "5" stampa Buzz
+    console.log("Fizz");
+    }else if(i % 5 === 0 ){                               // 1c. per i multipli di "3" e "5" stampa FizzBuzz
+    console.log("Buzz");
+    }else {
+    console.log(i);        
+    }                   
 }
